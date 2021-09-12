3c3351936703c34c076d09893b465eb8194e85bc
wenjin1997 <wenjin1997@126.com>
2021-09-13 07:44:31 +0800

    更新第5章作业

---
+++ code/chapter05/src/Demo.java
@@ -0,0 +1,16 @@
+/**
+ * @author 文进
+ * @version 1.0
+ */
+public class Demo {
+    public static void main(String[] args) {
+        int m = 0, n = 3;
+        if (m > 0) {
+            if (n > 2) {
+                System.out.println("OK1");
+            } else {
+                System.out.println("OK2");
+            }
+        }
+    }
+}
